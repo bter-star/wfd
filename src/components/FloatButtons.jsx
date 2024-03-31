cd8b081dbc28e6eeeb528e6dfd4dd89fe965ba28
Benjamin Terebelo <bterebelo64@gmail.com>
2024-03-30 23:09:33 -0400

    modified index

--- src/components/FloatButtons.jsx
+++ src/components/FloatButtons.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const FloatingButtons = () => {
   const navigate = useNavigate();
   return (
-    <div className="fixed z-50 -right-14 top-1/2 transform -translate-y-1/2 flex  -rotate-90 gap-2 text-sm">
+    <div className="fixed z-30 -right-14 top-1/2 transform -translate-y-1/2 flex  -rotate-90 gap-2 text-sm">
       <button
         onClick={() => navigate("/membership")}
         className="border border-tertiary-300 bg-tertiary-100  text-white px-4 py-2 rounded-t-sm"
